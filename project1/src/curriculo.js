f343174430697ed61f2c41fb87afa2c48f20c8fc
Tiago Neves <rykarumstuler@gmail.com>
2021-04-06 23:49:21 -0300

    Transformando as redes sociais em butoes que geram alertas nativos

--- project1/src/curriculo.js
+++ project1/src/curriculo.js
@@ -1,23 +1,76 @@
 import React from 'react';
-import { Image, StyleSheet, View, Text } from 'react-native';
+import { Image, StyleSheet, View, Text, ScrollView, Alert, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import foto from './assets/me.jpeg';
 
+
+
 const App = () => {
+
+    function handleRedeSocial(rede_social) {
+        switch (rede_social) {
+            case 'linkedin':
+                Alert.alert('Meu linkedin: ', 'https://linkedin.com/in/tiago-m-neves')
+                break
+            case 'github':
+                Alert.alert('Meu github: ', 'https://github.com/TiagoNeve')
+                break
+            case 'mail':
+                Alert.alert('Meu gmail: ', 'tiagomneves.52@gmail.com')
+                break
+        }
+    }
+
     return (
         <>
-            <View style={style.page}>
-                <View style={style.containerCabecalho}>
-                    <Image source={foto} style={style.foto} />
-                    <Text style={style.nome}>TIAGO MACIEL NEVES</Text>
-                    <Text style={style.funcao}>Desenvolvedor mobile</Text>
-                    <View style={style.redesSociais}>
-                        <Icon name="github" size={30} />
-                        <Icon name="facebook" size={30} />
-                        <Icon name="linkedin" size={30} />
+            <ScrollView>
+                <View style={style.page}>
+                    <View style={style.containerCabecalho}>
+                        <Image source={foto} style={style.foto} />
+                        <Text style={style.nome}>TIAGO MACIEL NEVES</Text>
+                        <Text style={style.funcao}>Desenvolvedor mobile</Text>
+                        <View style={style.redesSociais}>
+                            <TouchableOpacity onPress={() => handleRedeSocial('github')}>
+                                <Icon name="github" size={30} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleRedeSocial('linkedin')}>
+                                <Icon name="linkedin" size={30} />
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => handleRedeSocial('mail')}>
+                                <Icon name="mail" size={30} />
+                            </TouchableOpacity>
+                        </View>
                     </View>
+
+                    <View style={style.cardContainer}>
+                        <View style={style.card}>
+                            <View style={style.cardHeader}>
+                                <Text>Experiência profissional</Text>
+                            </View>
+                            <View style={style.cardContent}>
+                                <Text style={style.cardContentText}>Em desenvolvimento</Text>
+                                <Text style={style.cardContentText}>Apenas de CURSOS</Text>
+                                <Text style={style.cardContentText}>De grão em grão o galo enche o papo</Text>
+                            </View>
+                        </View>
+                    </View>
+
+                    <View style={style.cardContainer}>
+                        <View style={style.card}>
+                            <View style={style.cardHeader}>
+                                <Text>Formação Acadêmica</Text>
+                            </View>
+                            <View style={style.cardContent}>
+                                <Text style={style.cardContentText}>Cursos, muito cursos</Text>
+                                <Text style={style.cardContentText}>Analise e desenvolvimento de sistemas</Text>
+                                <Text style={style.cardContentText}>Universidade Estácio</Text>
+                            </View>
+                        </View>
+                    </View>
+
                 </View>
-            </View>
+            </ScrollView>
+
         </>
     )
 }
@@ -25,7 +78,7 @@ const App = () => {
 const style = StyleSheet.create({
     page: {
         flex: 1,
-        backgroundColor: '#E7E7E7'
+        backgroundColor: '#E7E7E7',
     },
     containerCabecalho: {
         marginTop: 50,
@@ -54,7 +107,28 @@ const style = StyleSheet.create({
         justifyContent: 'space-between',
         width: '60%',
         marginTop: 20,
-    }
+    },
+    cardContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    card: {
+        width: '60%',
+        borderRadius: 5,
+        borderWidth: 2,
+        borderColor: '#939393',
+        padding: 10,
+        backgroundColor: '#FFF',
+    },
+    cardContent: {
+        marginTop: 20,
+    },
+    cardContentText: {
+        color: '#939393',
+        marginBottom: 10
+    },
 })
 
 export default App;
